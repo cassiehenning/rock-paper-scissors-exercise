3f8ad3441462e46265ba0233ce983f288d97453f
cassiehenning <cjh281@georgetown.edu>
2021-02-03 17:40:04 -0500

    ask user for an input

---
+++ game.py
@@ -0,0 +1,27 @@
+print("Rock, Paper, Scissors, Shoot!")
+
+print("-------------------")
+print ("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print ("-------------------")
+
+# asking user for an input
+
+x = input("please choose either 'rock', 'paper', or 'scissors': rock")
+
+print(x)
+
+#once it reaches the exit statement, it will stop running the code
+#can be used to help fix errors
+
+print("you chose: ", x)
+
+# simulating a computer input 
+
+print("The computer chose: 'paper'")
+
+# determining who won
+
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("thanks for playing. Please play again!")
