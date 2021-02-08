1615839815ce72843c59a6186e9585755c38b1c6
cassiehenning <cjh281@georgetown.edu>
2021-02-08 16:18:39 -0500

    determine a winner

--- game2.py
+++ game2.py
@@ -8,23 +8,39 @@ print("-------------------")
 
 #asking user for an input
 
-x = input("Please choose either 'rock', 'paper', or 'scissors':")
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors':")
 
-print(x)
+#print(x)
+#print("You chose: ", x)
+print(f"You chose: {user_choice}")
 
-print("You chose: ", x)
-
-print(f"You chose: {x}")
-
-exit()
 
 #simulating a computer input
 
+import random 
 
-print("The computer chose: 'paper'")
+options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options)
+
+print(f"The computer chose: {computer_choice}")
 
 #determining who won
 
+
+#if UserChoice == ComputerChoice:
+        print("It's tie!")
+   elif UserChoice == "paper" and ComputerChoice == "rock":
+        print("You win! Congrats")
+    elif UserChoice == "paper" and ComputerChoice == "scissors":
+        print("Oh! The computer won, that's ok!")
+    elif UserChoice == "rock" and ComputerChoice == "paper":
+        print("Oh! The computer won, that's ok!")
+    elif UserChoice == "rock" and ComputerChoice == "scissors":
+        print("You win! Congrats")
+    elif UserChoice == "scissors" and ComputerChoice == "paper":
+        print("You win! Congrats")
+    elif UserChoice == "scissors" and ComputerChoice == "rock":
+        print("Oh! The computer won, that's ok!")
 
 print("-------------------")
 print("Oh, the computer won. It's ok.")
